c143e4aa46ce9ec239ede3ea79ab6adbcae5f633
healyyyyyy <54723263+healyyyyyy@users.noreply.github.com>
2020-12-08 00:01:12 +0500

    Used a better algorithm, trashed the old one

--- Algorithms/BellmanFord.py
+++ Algorithms/BellmanFord.py
@@ -1,50 +1,21 @@
-import os
-import sys
-import datetime
+def bellman_ford(graph, source):
+    # Step 1: Prepare the distance and predecessor for each node
+    distance, predecessor = dict(), dict()
+    for node in graph:
+        distance[node], predecessor[node] = float('inf'), None
+    distance[source] = 0
 
-begin_timer = datetime.datetime.now()
+    # Step 2: Relax the edges
+    for _ in range(len(graph) - 1):
+        for node in graph:
+            for neighbour in graph[node]:
+                # If the distance between the node and the neighbour is lower than the current, store it
+                if distance[neighbour] > distance[node] + graph[node][neighbour]:
+                    distance[neighbour], predecessor[neighbour] = distance[node] + graph[node][neighbour], node
 
-class Graph:
-    def __init__(self, vertices):  
-        self.V = vertices
-        self.graph = []  
-  
-    def addEdge(self, u, v, w):  
-        self.graph.append([u, v, w])  
-          
-    def printArr(self, dist):  
-        print("Vertex Distance from Source")  
-        for i in range(self.V):  
-            print("{0}\t\t{1}".format(i, dist[i]))
-
-    def BellmanFord(self, src):   
-        dist = [float("Inf")] * self.V  
-        dist[src] = 0
-  
-        for _ in range(self.V - 1):  
-            for u, v, w in self.graph:  
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]:  
-                        dist[v] = dist[u] + w
-                        
-        for u, v, w in self.graph:  
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]:  
-                        print("Graph contains negative weight cycle") 
-                        return
-
-        self.printArr(dist)  
-
-g = Graph(5)
-g.addEdge(0, 1, -1)  
-g.addEdge(0, 2, 4)  
-g.addEdge(1, 2, 3)  
-g.addEdge(1, 3, 2)  
-g.addEdge(1, 4, 2)  
-g.addEdge(3, 2, 5)  
-g.addEdge(3, 1, 1)  
-g.addEdge(4, 3, -3)  
-  
-g.BellmanFord(0)
-
-end_timer = datetime.datetime.now()
-running_time = (end_timer - begin_timer).total_seconds()
-print(running_time)
+    # Step 3: Check for negative weight cycles
+    for node in graph:
+        for neighbour in graph[node]:
+            assert distance[neighbour] <= distance[node] + graph[node][neighbour], "Negative weight cycle."
+ 
+    return distance, predecessor
